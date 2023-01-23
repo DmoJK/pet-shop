da65336db94cc83bae64a6c3f5b52f4a1908c4dd
DmoJK <l1sharook@mail.ru>
2023-01-23 12:10:04 +0300

    add hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { setupStore } from './store/store';
 
@@ -12,9 +12,9 @@ const store = setupStore()
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
